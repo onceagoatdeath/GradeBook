ca0d0869500775da95c9e2a117b46c9bd1cd56d7
Wiktor Paszewski <paszewski.wiktor@gmail.com>
2023-04-04 22:52:46 +0200

    Edited BaseGB Added RankedGB Added StandardGB

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -13,7 +13,7 @@ namespace GradeBook.GradeBooks
     {
         public string Name { get; set; }
         public List<Student> Students { get; set; }
-
+        public GradeBookType Type { get; set; }
         public BaseGradeBook(string name)
         {
             Name = name;
@@ -82,12 +82,31 @@ namespace GradeBook.GradeBooks
                 return null;
             }
 
+            BaseGradeBook gradeBook;
+
+
             using (var file = new FileStream(name + ".gdbk", FileMode.Open, FileAccess.Read))
             {
                 using (var reader = new StreamReader(file))
                 {
                     var json = reader.ReadToEnd();
-                    return ConvertToGradeBook(json);
+                    var jobject = JsonConvert.DeserializeObject<JObject>(json);
+                    var type = Enum.Parse(typeof(GradeBookType),jobject.GetValue("Type").ToString(),true);
+                    switch (type)
+                    {
+                        case GradeBookType.Standard:
+                            gradeBook = JsonConvert.DeserializeObject<StandardGradeBook>(json);
+                            break;
+                        case GradeBookType.Ranked:
+                            gradeBook = JsonConvert.DeserializeObject<RankedGradeBook>(json);
+                            break;
+                        default:
+                            gradeBook = JsonConvert.DeserializeObject<StandardGradeBook>(json);
+                            break;
+
+
+                    }
+                    return gradeBook;
                 }
             }
         }
---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,61 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Linq;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked grading requires min 5 students.");
+
+            var treshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (grades[treshold - 1] <= averageGrade)
+                return 'A';
+            else if (grades[(treshold * 2) - 1] <= averageGrade)
+                return 'B';
+            else if (grades[(treshold * 3) - 1] <= averageGrade)
+                return 'C';
+            else if (grades[(treshold * 4) - 1] <= averageGrade)
+                return 'D';
+            else
+                return 'F';
+
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires min 5 students.");
+                
+                return;
+            }
+
+
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires min 5 students.");
+                return;
+            }
+
+            base.CalculateStudentStatistics(name);
+        }
+
+
+    }
+}
